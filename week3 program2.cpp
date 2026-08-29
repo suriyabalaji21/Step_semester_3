137ee70df93299d1f9fb3b18a9a36c4c6ce82b45
suriyabalaji21 <suriyabalaji21off@gmail.com>
2026-08-29 06:22:03 +0530

    Update week3 program2.cpp

--- week3 program2.cpp
+++ week3 program2.cpp
@@ -1,19 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    bool isprime = true;
-    cin>>n;
-    for(int i =2;i<n;i++){
-        if(n%i ==0){
-           isPrime =false;
-           break;
 
-            }
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "The right-angled triangle pattern for " << rows << " rows is:" << endl;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
         }
-        if(isPrime)
-           cout<<"Prime";
-        else
-           cout<<"Not Prime";
-        return 0;
-    }l
+        cout << endl;
+    }
+
+    return 0;
+}
