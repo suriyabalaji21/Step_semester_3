6a30e4864b0e13781839ce9efbc43d4949c94c3e
suriyabalaji21 <suriyabalaji21off@gmail.com>
2026-09-18 06:35:37 +0530

    Add files via upload

---
+++ Practice/week5 practice p2.java
@@ -0,0 +1,16 @@
+class Problem2 {
+    static String findDuplicateTeam(String[] teamNames) {
+        for (int i = 0; i < teamNames.length; i++) {
+            for (int j = i + 1; j < teamNames.length; j++) {
+                if (teamNames[i].equals(teamNames[j]))
+                    return "Duplicate Found: " + teamNames[i];
+            }
+        }
+        return "No Duplicates Found";
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"ByteForce", "CodeCrafters", "ByteForce"};
+        System.out.println(findDuplicateTeam(names));
+    }
+}
