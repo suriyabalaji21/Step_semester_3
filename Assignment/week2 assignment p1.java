feefa88e78ddbb4bf890057525a59d1ae00a5104
suriyabalaji21 <suriyabalaji21off@gmail.com>
2026-09-18 06:32:11 +0530

    Add files via upload

---
+++ Assignment/week2 assignment p1.java
@@ -0,0 +1,7 @@
+public static void checkPinLength(String pin) {
+        if (pin.length() != 4) {
+            System.out.println("Invalid PIN — must be exactly 4 digits.");
+        } else {
+            System.out.println("PIN length OK.");
+        }
+    }
